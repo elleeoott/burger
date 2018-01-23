39046e2d97ca2060296c69034b918d11276a54a9
Evan Darst <evan.l.darst@gmail.com>
2018-01-22 23:56:18 -0500

    ORM management pseudo

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+//require a mySQL connection
+
+var connect = require("./connection.js");
+
+//function to push to mySQL
+
+//create ORM object that performs queries
+
+//Export to other sheets
+
+module.exports = orm;
