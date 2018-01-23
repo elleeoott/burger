9f8725a4b69136f1c3ed2bd842e7f96c7c3fadaa
Evan Darst <evan.l.darst@gmail.com>
2018-01-22 20:32:47 -0500

    initializing our routes

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+//setting handlebars for view
+
+var handlebars = require("express-handlebars");
+
+//get routes
+
+var routes = require("./controllers.burgers_controller.js");
+
+//use our routes
+
+app.use("/", routes);
+
 //listener
 
 app.listen(port);
