e58a16ddfecd279059805d29d0a415def5136523
Evan Darst <evan.l.darst@gmail.com>
2018-01-22 21:15:03 -0500

    added placeholder data for table burgers

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- placeholder data for burgers in our burgers_db
+
+INSERT INTO burgers (name_of_burger, is_eaten) VALUES ("Double Cheeseburger", false);
+INSERT INTO burgers (name_of_burger, is_eaten) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (name_of_burger, is_eaten) VALUES ("Chorizo Burger", false);
+INSERT INTO burgers (name_of_burger, is_eaten) VALUES ("Chicken Burger", false);
+INSERT INTO burgers (name_of_burger, is_eaten) VALUES ("Shrimp Patty Burger", true);
