36f921d6eee733d50df0a7f1d23ae684a0bf1bd6
Evan Darst <evan.l.darst@gmail.com>
2018-01-24 09:02:30 -0500

    pseudo for orm.js

--- config/orm.js
+++ config/orm.js
@@ -6,6 +6,17 @@ var connect = require("./connection.js");
 
 //create ORM object that performs queries
 
+var orm = {
+
+    //reuturn rows from the table
+
+    //then return results
+
+    //new function to add new entry to the table
+
+    //new function to update one entry in the table
+}
+
 //Export to other sheets
 
 module.exports = orm;
